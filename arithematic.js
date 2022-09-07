ecc577aeeca79be725ddaf03faadc182c4e48ba0
shivamofficial <91952992+shivamofficial@users.noreply.github.com>
2022-09-07 10:57:15 +0530

    Create arithematic.js

---
+++ arithematic.js
@@ -0,0 +1,18 @@
+let x=10;
+let y=20;
+let z=x+y;
+console.log(z);
+console.log(x-y);
+console.log(x*y)
+console.log(x/y);
+console.log(y%x);
+x++;
+console.log(x);
+x++;
+console.log(x);
+++x;
+console.log(x);
+y--;
+console.log(y);
+--y;
+console.log(y);
