40990ad958397b86ef52d0dc1076f68990c860e7
shivamofficial <91952992+shivamofficial@users.noreply.github.com>
2022-09-07 11:33:51 +0530

    Create single-doublequotes.js

---
+++ single-doublequotes.js
@@ -0,0 +1,15 @@
+let carName1 = "Volvo XC60";   // Using double quotes
+let carName2 = 'Volvo XC60';   // Using single quotes
+console.log( carName1);
+console.log( carName2);
+
+var car="its's shivam car's";
+console.log(car);
+
+let answer1 = "It's alright";             // Single quote inside double quotes
+let answer2 = "He is called 'Johnny'";    // Single quotes inside double quotes
+let answer3 = 'He is called "Johnny"';    // Double quotes inside single quotes
+
+console.log(answer1);
+console.log(answer2);
+console.log(answer3);
