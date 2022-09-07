60cf8f900c8fc63dca8af24d2f4a6855a9d26210
shivamofficial <91952992+shivamofficial@users.noreply.github.com>
2022-09-07 12:44:09 +0530

    Create object.js

---
+++ object.js
@@ -0,0 +1,15 @@
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue"
+};
+
+You can access object properties in two ways:
+
+objectName.propertyName
+
+objectName["propertyName"]
+
+person.lastName;
+
