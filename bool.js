f432c20d8301eda012b6f05dbde75034f10d2f92
shivamofficial <91952992+shivamofficial@users.noreply.github.com>
2022-09-07 12:13:12 +0530

    Create bool.js

---
+++ bool.js
@@ -0,0 +1,23 @@
+let x=10;
+let y=20;
+console.log(x==y);
+console.log(typeof(x));
+console.log(typeof(y));
+console.log(x===y);
+
+
+let a=10;
+let b=10;
+console.log(a==b);
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(a===b);
+
+false
+number
+number
+false
+true
+number
+number
+true
