ab5947c04446f605698882a96d405e020a30f562
shivamofficial <91952992+shivamofficial@users.noreply.github.com>
2022-09-08 15:50:30 +0530

    Create string.js

---
+++ string.js
@@ -0,0 +1,14 @@
+// x is a string
+let x = "John";
+
+// y is an object
+let y = new String("John");
+
+console.log(typeof(x));
+console.log(typeof(y));
+console.log(x==y);
+console.log(x===y);
+
+let z=new String("john");
+console.log(y==z);
+console.log(y===z);
