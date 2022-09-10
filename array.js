1864cebb15053f0245c59a280d0b2cc5539b876d
shivamofficial <91952992+shivamofficial@users.noreply.github.com>
2022-09-10 16:08:20 +0530

    Create array.js

---
+++ array.js
@@ -0,0 +1,62 @@
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits);
+
+// console.log(fruits.pop());
+
+// console.log(fruits);
+// console.log(fruits.push("kiwi"));
+
+// console.log(fruits);
+
+// console.log(fruits.shift());
+// console.log(fruits);
+
+// console.log(fruits.unshift("papaya"));
+// console.log(fruits);
+
+// //console.log(delete.fruits[0]);
+// console.log(fruits.toString());
+
+// console.log(fruits.join("*"));
+
+
+// const myGirls = ["Cecilie", "Lone"];
+// const myBoys = ["Emil", "Tobias", "Linus"];
+
+// console.log(myGirls.concat(myBoys));
+
+// const fruit = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// console.log(fruit.splice(2,2));
+
+// console.log(fruit.slice(2));
+
+// console.log(fruit);
+
+// const fruitss = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruitss.slice(1, 3);
+// console.log(citrus);
+// console.log(fruitss.slice(2));
+
+
+
+output....................................
+[ 'Banana', 'Orange', 'Apple', 'Mango' ]
+Mango
+[ 'Banana', 'Orange', 'Apple' ]
+4
+[ 'Banana', 'Orange', 'Apple', 'kiwi' ]
+Banana
+[ 'Orange', 'Apple', 'kiwi' ]
+4
+[ 'papaya', 'Orange', 'Apple', 'kiwi' ]
+papaya,Orange,Apple,kiwi
+papaya*Orange*Apple*kiwi
+[ 'Cecilie', 'Lone', 'Emil', 'Tobias', 'Linus' ]
+[ 'Lemon', 'Apple' ]
+[ 'Mango' ]
+[ 'Banana', 'Orange', 'Mango' ]
+[ 'Lemon', 'Apple', 'Mango' ]
+
+
+
+
