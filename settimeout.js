3be5b9fec023fb4e283fef062eb1abe34c7aa4c0
shivamofficial <91952992+shivamofficial@users.noreply.github.com>
2022-09-19 16:18:21 +0530

    Create settimeout.js

---
+++ settimeout.js
@@ -0,0 +1,50 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+
+function x()
+{
+    for(var i=0;i<=10;i++)
+    {
+        setTimeout(function ()
+        {
+            console.log(i);
+        },i*1000);
+    }
+    console.log("Welcome to Programiz!");
+}
+x();
+
+// output 11
+
+
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+
+function x()
+{
+    for(let i=0;i<=10;i++)
+    {
+        setTimeout(function ()
+        {
+            console.log(i);
+        },i*1000);
+    }
+    console.log("Welcome to Programiz!");
+}
+x();
+
+//
+Welcome to Programiz!
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
