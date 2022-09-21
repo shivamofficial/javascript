085922f02a4bf6b41aa32abe28a6349f4976423d
shivamofficial <91952992+shivamofficial@users.noreply.github.com>
2022-09-21 21:12:19 +0530

    Create apply.js

---
+++ apply.js
@@ -0,0 +1,19 @@
+ call() and apply() serve the exact same purpose. The only difference between how they work is that call() expects all parameters to be passed in individually,
+   whereas apply() expects an array of all of our parameters.
+
+
+var pokemon = {
+    firstname: 'Pika',
+    lastname: 'Chu ',
+    getPokeName: function() {
+        var fullname = this.firstname + ' ' + this.lastname;
+        return fullname;
+    }
+};
+
+var pokemonName = function(snack, hobby) {
+    console.log(this.getPokeName() + ' loves ' + snack + ' and ' + hobby);
+};
+
+pokemonName.call(pokemon,'sushi', 'algorithms'); // Pika Chu  loves sushi and algorithms
+pokemonName.apply(pokemon,['sushi', 'algorithms']); // Pika Chu  loves sushi and algorithms
